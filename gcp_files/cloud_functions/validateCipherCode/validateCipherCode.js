c96eafe08c9a39e8f916820a711f4dd4057e6dde
Meha Vijaybhai Desai <mh305558@dal.ca>
2022-07-22 17:14:40 -0300

    Updated endpoint for report management

--- gcp_files/cloud_functions/validateCipherCode/validateCipherCode.js
+++ gcp_files/cloud_functions/validateCipherCode/validateCipherCode.js
@@ -1,3 +1,113 @@
+// /**
+//  * Responds to any HTTP request.
+//  *
+//  * @param {!express:Request} req HTTP request context.
+//  * @param {!express:Response} res HTTP response context.
+//  */
+
+// const Firestore = require('@google-cloud/firestore');
+// const PROJECTID = 'serverless-bnb-6c350';
+// const COLLECTION_NAME = 'user_data';
+// const firestore = new Firestore({
+//     projectId: PROJECTID,
+//     timestampsInSnapshots: true
+// });
+
+// // source: https://gist.github.com/EvanHahn/2587465
+// var caesarShift = function (str, amount) {
+//     // Wrap the amount
+//     if (amount < 0) {
+//         return caesarShift(str, amount + 26);
+//     }
+
+//     // Make an output variable
+//     var output = "";
+
+//     // Go through each character
+//     for (var i = 0; i < str.length; i++) {
+//         // Get the character we'll be appending
+//         var c = str[i];
+
+//         // If it's a letter...
+//         if (c.match(/[a-z]/i)) {
+//             // Get its code
+//             var code = str.charCodeAt(i);
+
+//             // Uppercase letters
+//             if (code >= 65 && code <= 90) {
+//                 c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+//             }
+
+//             // Lowercase letters
+//             else if (code >= 97 && code <= 122) {
+//                 c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+//             }
+//         }
+
+//         // Append
+//         output += c;
+//     }
+
+//     // All done!
+//     return output;
+// };
+
+// exports.ValidateCipherCode = (req, res) => {
+//     var request = req.body;
+//     let challengeText = request.challengeText; // TODO from request
+//     let userCipherAnswer = request.cipherAnswer; // TODO from request
+
+//     res.set('Access-Control-Allow-Origin', '*');
+
+//     if (req.method === 'OPTIONS') {
+//         // Send response to OPTIONS requests
+//         res.set('Access-Control-Allow-Methods', 'GET');
+//         res.set('Access-Control-Allow-Headers', 'Content-Type');
+//         res.set('Access-Control-Max-Age', '3600');
+//         res.status(204).send('');
+//     } else {
+//         return firestore.collection(COLLECTION_NAME)
+//             .doc("registration_data")
+//             .get()
+//             .then(doc => {
+//                 if (!(doc && doc.exists)) {
+//                     return res.status(404).send({
+//                         error: 'Unable to find the document'
+//                     });
+//                 }
+//                 const data = doc.data();
+//                 if (!data) {
+//                     return res.status(404).send({
+//                         error: 'Found document is empty'
+//                     });
+//                 }
+//                 const userDataFromFirestore = data[request.email];
+//                 let decryptedCipher = caesarShift(challengeText, Number(userDataFromFirestore['cipherShiftKey']));
+//                 if (decryptedCipher === request['cipherAnswer']) {
+//                     return res.status(200).send({
+//                         status: 200,
+//                         message: 'User is valid',
+//                         valid: true,
+//                         decryptedCipher: decryptedCipher
+//                     });
+//                 }
+
+//                 return res.status(200).send({
+//                     status: 200,
+//                     message: 'User is invalid',
+//                     valid: false,
+//                     decryptedCipher: decryptedCipher
+//                 });
+//             }).catch(err => {
+//                 console.error(err);
+//                 return res.status(404).send({
+//                     error: 'Unable to retrieve the document',
+//                     err
+//                 });
+//             });
+//     }
+// };
+
 /**
  * Responds to any HTTP request.
  *
@@ -8,6 +118,7 @@
 const Firestore = require('@google-cloud/firestore');
 const PROJECTID = 'serverless-bnb-6c350';
 const COLLECTION_NAME = 'user_data';
+const request = require('request'); // for post request @Meha
 const firestore = new Firestore({
     projectId: PROJECTID,
     timestampsInSnapshots: true
@@ -52,6 +163,30 @@ var caesarShift = function (str, amount) {
     return output;
 };
 
+
+
+
+// new endpoint for updating logging reports. @Meha
+var manageUserLogs = function (email) {
+    var datetime = new Date();
+    var data = { "email": email, "date": datetime, "action": "login" }
+
+    request(
+        {
+            url: "",
+            method: "POST",
+            json: true,
+            body: data
+        },
+        function postLogs(error, response, body) {
+            console.log(response)
+        }
+    )
+
+}
+
+
+
 exports.ValidateCipherCode = (req, res) => {
     var request = req.body;
     let challengeText = request.challengeText; // TODO from request
@@ -84,7 +219,9 @@ exports.ValidateCipherCode = (req, res) => {
                 const userDataFromFirestore = data[request.email];
                 let decryptedCipher = caesarShift(challengeText, Number(userDataFromFirestore['cipherShiftKey']));
                 if (decryptedCipher === request['cipherAnswer']) {
+                    manageUserLogs(request.email); // new @Meha
                     return res.status(200).send({
+
                         status: 200,
                         message: 'User is valid',
                         valid: true,
@@ -107,5 +244,6 @@ exports.ValidateCipherCode = (req, res) => {
             });
     }
 };
+
 
 
